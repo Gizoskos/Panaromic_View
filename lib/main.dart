beee9da0732dcfcbaaf0b19f8bf51e8964bd8f5c
Gizoskos <gizemgultoprak2004@gmail.com>
2024-05-02 15:16:49 +0300

    Panaroma viewer has completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:panorama_viewer/panorama_viewer.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +10,66 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<PanoramaState> _widgetInstanceKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PanoramaViewer(
+              key: _widgetInstanceKey,
+              child: Image.asset('assets/images/1.jpg')),
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 3.0),
+                  child: IconButton.filledTonal(
+                    onPressed: () {
+                      final currentState = _widgetInstanceKey.currentState;
+                      if (currentState != null) {
+                        final currentZoom = currentState.scene!.camera.zoom;
+                        currentState.setZoom(currentZoom + 0.3);
+                      }
+                    },
+                    icon: const Icon(Icons.add),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 3.0),
+                  child: IconButton.filledTonal(
+                    onPressed: () {
+                      final currentState = _widgetInstanceKey.currentState;
+                      if (currentState != null) {
+                        final currentZoom = currentState.scene!.camera.zoom;
+                        currentState.setZoom(currentZoom - 0.3);
+                      }
+                    },
+                    icon: const Icon(Icons.remove),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
